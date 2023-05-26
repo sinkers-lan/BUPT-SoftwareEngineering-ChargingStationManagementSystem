3a8d114371e13bfe9f62a7e110e4406f2ab1270d
sinkers-lan <18810170746@163.com>
2023-05-26 13:27:16 +0800

    更新后端文件层次

---
+++ fastapi/domain/user.py
@@ -0,0 +1,7 @@
+class QInfo:
+    def __init__(self, mode, user_id, degree, start_time):
+        self.mode = mode
+        self.user_id = user_id
+        self.degree = degree
+        self.start_time = start_time
+
