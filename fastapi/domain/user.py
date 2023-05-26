f9f709c1b2309718984febe9050cb6e215188c8d
sinkers-lan <18810170746@163.com>
2023-05-26 18:27:34 +0800

    Update user.py
    domain/user.py

--- fastapi/domain/user.py
+++ fastapi/domain/user.py
@@ -16,41 +16,62 @@ class QInfo:
 
 
 class QQueue:
-    def __init__(self, mode: str):
+    def __init__(self, mode: str, max_len):
         self.mode = mode
         self.len = 0
-        self.q: List[QInfo] = []
+        self.max_len = max_len
+        self.__q: List[QInfo] = []
 
     def get_your_len(self, car_id):
         i = 0
-        for q_info in self.q:
+        for q_info in self.__q:
             if q_info.car_id == car_id:
                 return i
             i += 1
         return -1
 
     def pop(self):
+        if self.len == 0:
+            print("pop error")
+            return
         self.len -= 1
-        return self.q.pop()
+        return self.__q.pop()
 
     def push(self, q_info: QInfo):
+        if self.len == self.max_len:
+            print("push error")
+            return
         self.len += 1
-        self.q.append(q_info)
+        self.__q.append(q_info)
 
 
 class ChargingStationQueue(QQueue):
-    def __init__(self, mode: str, ref: str):
-        super().__init__(mode)
-        self.ref = ref
+    def __init__(self, mode: str, name: str):
+        super().__init__(mode, 2)
+
+
+class ChargingStation:
+    def __init__(self, mode, name):
+        self.mode = mode
+        self.name = name
         if mode == "快充":
             self.speed = 30.0
         else:
             self.speed = 7.0
+        self.state = "空闲"
+        self.q_queue = ChargingStationQueue(mode, name)
+
+    def get_queue_len(self):
+        return self.q_queue.len
+
+    def add_car(self, q_info: QInfo):
+        self.q_queue.push(q_info)
+
 
 
 class WaitingQueue(QQueue):
     def __init__(self, mode: str):
-        super().__init__(mode)
+        super().__init__(mode, 6)
 
 
 class WaitingArea:
@@ -73,4 +94,20 @@ class WaitingArea:
             self.slow_queue.push(q_info)
             return self.slow_queue.len
 
-    # def inquire_car_state(self, ):
+
+
+class AllArea:
+    def __init__(self):
+        self.A = ChargingStation("快充", "A")
+        self.B = ChargingStation("快充", "B")
+        self.C = ChargingStation("慢充", "C")
+        self.D = ChargingStation("慢充", "D")
+        self.E = ChargingStation("慢充", "E")
+        self.waiting_area = WaitingArea()
+        self.q_infos: List[QInfo] = []
+
+
+    # def inquire_car_state(self, car_id):
+
+    def add_car(self, q_info: QInfo):
+        self.waiting_area.add_car(q_info)
